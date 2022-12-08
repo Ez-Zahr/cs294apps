c52acb0905d48ae6e06a8ec1ecba61705c98cc84
Aziz Alzahrani <77460156+Ez-Zahr@users.noreply.github.com>
2022-12-08 04:49:52 +0000

    Implemented the home screen

--- bigProject/script.js
+++ bigProject/script.js
@@ -42,11 +42,39 @@ function getCrashes() {
     }).catch(err => console.log(err));
 }
 
+const navBtn = document.querySelector("#nav-btn");
+
 function switchScreen(dest) {
   document.querySelector("#" + curScreen).style.display = "none";
   curScreen = dest;
-  document.querySelector("#" + curScreen).style.display = "block";
+  document.querySelector("#" + curScreen).style.display = "flex";
+  navBtn.innerText = (curScreen === "screen1") ? "menu" : "arrow_back";
 }
 
 let curScreen = "screen1";
-document.querySelector("#" + curScreen).style.display = "block";
+document.querySelector("#" + curScreen).style.display = "flex";
+
+const topAppBarEl = document.querySelector('.mdc-top-app-bar');
+const topAppBar = new mdc.topAppBar.MDCTopAppBar(topAppBarEl);
+topAppBarEl.addEventListener("MDCTopAppBar:nav", () => {
+  if (curScreen !== "screen1") switchScreen("screen1");
+});
+
+document.querySelectorAll('.mdc-list').forEach(listElem => {
+  const list = new mdc.list.MDCList(listElem);
+  list.listElements.map((listItemEl) => new mdc.ripple.MDCRipple(listItemEl));
+});
+
+document.querySelector("#screen1 .mdc-list").addEventListener("MDCList:action", (e) => {
+  switch (e.detail.index) {
+    case 0:
+      switchScreen("screen2");
+      break;
+    case 1:
+      switchScreen("screen3");
+      break;
+    case 2:
+      switchScreen("screen4");
+      break;
+  }
+});
